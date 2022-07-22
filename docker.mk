436217d22539ee17646275d38411cbb6009ff437
rensyct <80810999+rensyct@users.noreply.github.com>
2022-07-22 12:52:03 +0530

    Bump up dell-csi-operator version to v1.9.0 (#74)
    * Bump up operator version to v1.9.0
    
    * Fix container and image scan issue

--- docker.mk
+++ docker.mk
@@ -30,7 +30,7 @@ OPERATOR_IMAGE ?= dell-csi-operator
 BUNDLE_IMAGE ?= csiopbundle_ops_certified
 # Default Index Image name
 INDEX_IMAGE ?= dellemcregistry_certified
-SOURCE_INDEX_IMG ?= dellemc/dell-csi-operator/dellemcregistry_certified:v1.7.0
+SOURCE_INDEX_IMG ?= dellemc/dell-csi-operator/dellemcregistry_certified:v1.8.0
 
 
 # Operator Images
