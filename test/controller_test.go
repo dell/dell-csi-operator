acf885d3335f46317b74ac9c968a28d9ad6446fa
Rajshree Khare <Rajshree.Khare@dell.com>
2022-11-08 16:48:54 +0530

    updated version for UT files

--- test/controller_test.go
+++ test/controller_test.go
@@ -134,7 +134,7 @@ func (suite *ControllerTestSuite) SetupSuite() {
 			reconciler: &controllers.CSIVXFlexOSReconciler{
 				Log: ctrl.Log.WithName("controllers").WithName("CSIVXFlexOS"),
 			},
-			k8sVersion: "v123",
+			k8sVersion: "v125",
 			findCR: func(inObjects []runtime.Object) (string, string) {
 				for _, o := range inObjects {
 					if cr, ok := o.(*v1.CSIVXFlexOS); ok {
