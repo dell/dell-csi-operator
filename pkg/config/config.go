33caf14bf1d1bb165a7460764537b0990b590154
panigs7 <spandita.panigrahi@emc.com>
2022-11-21 13:25:41 -0500

    Fix lint issues

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,6 +11,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
+
 package config
 
 import csiv1 "github.com/dell/dell-csi-operator/api/v1"
